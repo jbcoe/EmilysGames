cc3a580f10db0d3e6e2464ef7df19f8558691263
Jonathan B. Coe <jbcoe@me.com>
2016-11-23 18:51:31 +0000

    fix with autopep8

--- words_with_emily_server.py
+++ words_with_emily_server.py
@@ -6,19 +6,21 @@ from flask import Flask, abort, redirect, url_for
 app = Flask(__name__)
 from jinja2 import Template
 
+
 @app.route("/game")
 def game():
-    word,matches = g.generate_word_and_matches()
+    word, matches = g.generate_word_and_matches()
     return Template("""
             <h1>{{word}} [{{score}}]</h1>
             <ul>
             {% for m in matches %}<li><a href="../match/{{word}}/{{m}}">{{m}}</a></li>{% endfor %}
             </ul>
-            """).render({'word':word, 'score':g.get_score(), 'matches':matches})
+            """).render({'word': word, 'score': g.get_score(), 'matches': matches})
+
 
 @app.route("/match/<word>/<match>")
 def match(word, match):
-    points,common = words_with_emily.calculate_points(word, match)
+    points, common = words_with_emily.calculate_points(word, match)
     g.increase_score(points)
     return redirect(url_for('game'))
 
